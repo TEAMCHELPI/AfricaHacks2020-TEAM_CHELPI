907c70ca212185a4a96193a7d090907d7dc1bbed
lil-armstrong <oebong1@gmail.com>
2020-12-08 22:24:12 +0100

    UI fixes

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-    <section class="dashboard-hero relative min-h-screen bg-gray-900"
-    :style="{backgroundImage: `url(${banner})`}">
-      <span class="bg-black opacity-75 absolute top-0 left-0 w-full h-full"></span>
+    <section class="dashboard-hero flex flex-col justify-end relative min-h-screen bg-gray-900"
+             :style="{backgroundImage: `url(${banner})`}">
+      <span class="bg-black z-0 opacity-75 absolute top-0 left-0 w-full h-full"></span>
+      <section class="container mx-auto mt-auto flex space-x-6 z-10 mb-12">
+        <section>
+          <span class="w-20 h-20 bg-white rounded-full block"></span>
+        </section>
+        <section>
+          <header class="mb-2"><h1 class="text-2xl text-white font-head font-medium">
+            Chef Leint
+          </h1>
+          <section>
+            <span></span>
+            <span></span>
+          </section>
+          </header>
+          <p class="text-gray-100 ">Meals at it best, get quality cooked, baked and grilled meals</p>
+          <small class="text-gray-200 font-light">Expected Meal Time Preparation: 10min - 20min</small>
+        </section>
+      </section>
     </section>
     <section class="container mx-auto">
       <meal-mansonry-grid></meal-mansonry-grid>
@@ -13,14 +30,15 @@
 
 <script>
 	import MealMansonryGrid from "~/components/MealMansonryGrid";
+
 	export default {
 		name: "index",
 		components: { MealMansonryGrid },
-    data(){
+		data() {
 			return {
 				banner: '/images/sample/meals/meal005.jpg'
-      }
-    },
+			}
+		},
 		layout: 'dashboard',
 		methods: {
 			openLeftSidebar() {
@@ -31,10 +49,10 @@
 </script>
 
 <style scoped>
-.dashboard-hero{
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-attachment: fixed;
-}
+  .dashboard-hero {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+  }
 </style>
