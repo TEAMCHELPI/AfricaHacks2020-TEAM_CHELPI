0f91b6f4e0d137f65f73edaecbca679bc8229ba6
Ebong, Okposong <31352135+lil-armstrong@users.noreply.github.com>
2020-12-09 06:40:24 +0100

    update TheSidebar.vue

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -2,22 +2,36 @@
   <aside class="w-full h-screen z-20 fixed top-0 transition-all duration-500"
          :style="{ maxWidth: '300px', left: isOpen ? '0' : '-100%' }"
          ref="">
-
+    
+<!-- Overlay -->
     <div class="opacity-75 fixed inset-0 z-40 bg-black" @click.stop="closeLeftSidebar"
          :class="['opacity-50 fixed inset-0 bg-black', { hidden: !isOpen }]"></div>
+<!-- End overlay -->
 
     <section class="flex flex-col justify-end bg-white z-50 relative" id="sidebar">
-
+      
+<!-- Close button -->
       <button class="z-40 absolute top-0 right-0 -mr-10" @click.stop="closeLeftSidebar">
         <span class="text-4xl text-white">&times;</span>
       </button>
+      <!-- End close button -->
+      
+      <!-- Top section -->
       <section class="p-4 justify-self-start mb-auto">
+        
+        <!-- User avatar -->
         <div class="user-avatar w-12 h-12 bg-white shadow-outline rounded-full relative"
         :style="{backgroundImage: `url(${avatar})`}">
           <span class="absolute w-3 h-3 rounded-full right-0 top-0 mt-2 -mr-1 bg-green-500"></span>
         </div>
+        <!-- End user avatar -->
+        
       </section>
+      <!-- End top section -->
+      
+      <!-- Bottom section -->
       <section class="p-4">
+        <!-- Navigation links -->
         <ul class="my-4 text-base leading-normal font-medium">
           <li class="mb-2">
             <n-link class="nav-link" :to="{name: 'dashboard-orders'}">My Orders</n-link>
@@ -26,8 +40,15 @@
             <n-link class="nav-link" :to="{name: 'dashboard-create-restaurant'}">Create Virtual Restaurant</n-link>
           </li>
         </ul>
-        <button class="my-4 uppercase p-4 w-full bg-brand-primary text-white font-medium">Logout</button>
+        <!-- End navigation links -->
+        
+        <!-- Logout button -->
+        <button class="my-4 uppercase p-4 w-full bg-brand-primary text-white font-medium">
+          Logout
+        </button>
+        <!-- End logout button -->
       </section>
+      <!-- End bottom section -->
     </section>
   </aside>
 </template>
